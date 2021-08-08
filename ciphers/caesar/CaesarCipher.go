75054c01a004b40bbf89ad19c10359c2c09f3b10
LIUZHENYU0609 <1329004166@qq.com>
2021-08-08 21:07:34 +0800

    fix: 删除凯撒加密算法中的冗余代码

--- ciphers/caesar/CaesarCipher.go
+++ ciphers/caesar/CaesarCipher.go
@@ -1,29 +1,28 @@
-// Package caesar is the shift cipher
-// ref: https://en.wikipedia.org/wiki/Caesar_cipher
 package caesar
 
-// Encrypt encrypts by right shift of "key" each character of "input"
+/*
+	凯撒加密算法：针对因为字母'a'-'z'，'A'-'Z',将字母向由迁移key个位置
+	假设字母为a，key=1，则加密转换后为字母'b'，对于其他英文字母就保持原来的值就好
+ */
 func Encrypt(input string, key int) string {
-	// if key is negative value,
-	// updates "key" the number which congruents to "key" modulo 26
+	// 为了避免key是负数或者key大于26或者小于-26，首先将key对26取模后加26再对26取模
 	key = (key%26 + 26) % 26
 
-	outputBuffer := []byte{}
+	var outputBuffer []byte
 	for _, r := range input {
-		newByte := byte(r)
-		if 'A' <= newByte && newByte <= 'Z' {
-			outputBuffer = append(outputBuffer, byte(int('A')+int(int(newByte-'A')+key)%26))
-		} else if 'a' <= newByte && newByte <= 'z' {
-			outputBuffer = append(outputBuffer, byte(int('a')+int(int(newByte-'a')+key)%26))
+		// r就是字母的数字编码形式
+		if 'A' <= r && r <= 'Z' {
+			outputBuffer = append(outputBuffer, byte('A' + (r - 'A' + int32(key))%26))
+		} else if 'a' <= r && r <= 'z' {
+			outputBuffer = append(outputBuffer, byte('a' + (r -'a' + int32(key))%26))
 		} else {
-			outputBuffer = append(outputBuffer, newByte)
+			outputBuffer = append(outputBuffer, byte(r))
 		}
 	}
 	return string(outputBuffer)
 }
 
-// Decrypt decrypts by left shift of "key" each character of "input"
+// Decrypt 解码就是向左迁移key，等价于向右迁移26-key个位置
 func Decrypt(input string, key int) string {
-	// left shift of "key" is same as right shift of 26-"key"
 	return Encrypt(input, 26-key)
 }
